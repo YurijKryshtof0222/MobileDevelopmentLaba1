ef31f6bf1e1a9750ee91fefa25e01f745dafb5af
Yurii <105464154+YurijKryshtof0222@users.noreply.github.com>
2023-10-15 12:28:05 +0300

    Update playground.swift

--- playground.swift
+++ playground.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+enum MathError: Error {
+    case divisionByZero
+    case operationIsNotSupported
+}
+
 func add(number1: Int?, number2: Int?) -> Int {
     return number1! + number2!
 }
@@ -12,35 +17,40 @@ func multiply(number1: Int?, number2: Int?) -> Int {
     return number1! * number2!
 }
 
-func divide(number1: Int?, number2: Int?) -> Int {
+func divide(number1: Int?, number2: Int?) throws -> Int {
     if number2! != 0 {
         return number1! / number2!
+    } else {
+        throw MathError.divisionByZero
     }
-    print("Error: cannot divide by zero!")
-    return 0
 }
 
-
-let operation: String? = "+"
+let operation: String? = "/"
 let number1: Int? = 5
-let number2: Int? = 7
+let number2: Int? = 0 // Set number2 to 0 to trigger division by zero
 
 var result: Int?
 
 if let operation = operation, let number1 = number1, let number2 = number2 {
-    switch operation {
-        case "+": 
-            result = add(number1: number1, number2: number2)
-        case "-":
-            result = subtract(number1: number1, number2: number2)
-        case "*":
-            result = multiply(number1: number1, number2: number2)
-        case "/":
-            result = divide(number1: number1, number2: number2)
-        default:
-            print("This operation is not supported!")
-            result = 0
-    }  
+    do {
+        switch operation {
+            case "+":
+                result = add(number1: number1, number2: number2)
+            case "-":
+                result = subtract(number1: number1, number2: number2)
+            case "*":
+                result = multiply(number1: number1, number2: number2)
+            case "/":
+                result = try divide(number1: number1, number2: number2)
+            default:
+                throw MathError.operationIsNotSupported
+        }
+        print("\(number1) \(operation) \(number2) = \(result)")
+    } catch MathError.divisionByZero {
+        print("Error: cannot divide by zero!")
+    } catch MathError.operationIsNotSupported {
+        print("Error: Operation \(operation) is not supported!")
+    } catch {
+        print("Error: An unexpected error occurred: \(error)")
+    }
 }
-
-print("\(number1!) \(operation!) \(number2!) = \(result!)")
